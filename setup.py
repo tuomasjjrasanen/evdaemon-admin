07fde27b06caf405961b8b5f611a5d1728dc5e95
Tuomas Räsänen <tuos@codegrove.org>
2010-02-25 13:45:51 +0200

    Install .desktop file and icon correctly.

--- setup.py
+++ setup.py
@@ -2,19 +2,25 @@
 
 from distutils.core import setup
 
-setup(name='evdaemon-admin',
-      version='0.1',
-      description='Tools for evdaemon administration.',
-      long_description='Tools for evdaemon administration.',
-      author='Tuomas Räsänen',
-      author_email='tuos@codegrove.org',
-      url='http://codegrove.org/evdaemon-admin/',
+setup(name="evdaemon-admin",
+      version="0.1",
+      description="Tools for evdaemon administration.",
+      long_description="Tools for evdaemon administration.",
+      author="Tuomas Räsänen",
+      author_email="tuos@codegrove.org",
+      url="http://codegrove.org/evdaemon-admin/",
       package_dir={
-        'evdaemon': 'src/lib',
+        "evdaemon": "src/lib",
         },
       scripts=[
-        'src/bin/evdaemon-admin-gtk',
+        "src/bin/evdaemon-admin-gtk",
         ],
-      data_files=[('bin', ['src/bin/evdaemon-admin-gtk.xml']),],
-      packages=['evdaemon'],
+      data_files=[("bin",
+                   ["src/bin/evdaemon-admin-gtk.xml"]),
+                  ("/usr/share/applications",
+                   ["share/applications/evdaemon-admin.desktop"]),
+                  ("/usr/share/pixmaps",
+                   ["share/pixmaps/evdaemon-admin.xpm"]),
+                  ],
+      packages=["evdaemon"],
       )
